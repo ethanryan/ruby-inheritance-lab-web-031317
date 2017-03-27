f9c8508cc0657e3fb4228703ef4350bcbfd2c48e
ethanryan <ethanryan@gmail.com>
2017-03-26 22:34:51 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require "pry"
+
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    #@last_name = last_name
+    @knowledge = []
+  end
+
+  def learn(string)
+    #binding.pry
+    @knowledge << string
+  end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+
+  attr_accessor :first_name, :last_name, :knowledge #adding knowledge method
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+end
